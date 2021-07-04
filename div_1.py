99f468be8e7311f713d59556beec93e550bbb751
FluffyXVII <71878951+FluffyXVII@users.noreply.github.com>
2021-07-04 13:10:58 +0530

    Update div_1.py

--- div_1.py
+++ div_1.py
@@ -1,8 +1,13 @@
 #aka Gen module
 from random_username.generate import generate_username
-print(generate_username(5))
+list(generate_username(10000000))
 
+print("The End")
+
+"""
 def gen():
     return 5
 
 if __name__ == "__main__":pass
+"""
+
