b4fb31aca2e4e156ccb2a174950973e8b08ca732
doubl-u <69413572+doubl-u@users.noreply.github.com>
2021-07-06 20:36:02 +0530

    :)

---
+++ basics_test.py
@@ -0,0 +1,59 @@
+#all functions should run in a loop repeatedly unless the keyword "Iquit:P" is typed in at any point in time. no external online help or external libraries :)
+"""
+Q_1, 
+Make a calculator using the 3 user inputs (1st number, 2nd number, and the operation needed to be sunducted between them)
+
+The user needs to be able to add, subtract, multiply, divide, raise them to a interger (powers) and root them to a integer
+
+ex. 8 of root 3 = 2
+
+"""
+#completed the captcha test to check if you are human.
+"""
+Q_2, 
+Make a script that takes the a string from the user and switches the capitalisation of alphabets in the string alternatively(with first letter capital).
+
+ex. "hello world 123four" = "HeLlO wOrLd 123FoUr"
+
+"""
+#meh
+"""
+Q_3,
+Make a visual multiplication table with the square dimension of user specification (11 will make a 11x11 table).
+This table should not display any square numbers (1 is square number as 1x1 = 1, with "LoL" in its empty place).
+This table also should be positioned in the way that the smallest value is in the top-right corner and the highest value in the bottom-right corner.
+
+The table should only show numbers and it should have all first digits of the numbers perfectly aligned in the xy-axis, no matter even if there are numbers range from 1 to 4 digits in different places.
+(including aligning the first L of LoL)
+
+ex. with user input 4,
+    
+    4    3    2    LoL
+    8    6    LoL  2
+    12   LoL  6    3
+    LoL  12   8    4
+
+"""
+#testing out whether logical part of your brain is running
+"""
+Q_4,
+Make a database for all numbers until the user specification,
+The information should be revealed on user calling only (cannot display everything at once)
+The information stored should contain: whether if its an odd number or even number, whether if its a prime number (1 is not prime), 
+and whether or not the current number inputed is the same as the last number inquired for (its a default no on the first try)
+
+ex. with maximum set to 100,
+>>3
+    1. odd or even?
+    2. prime number?
+    3. same shit again?
+>>1
+    odd
+>>1
+    1. odd or even?
+    2. prime number?
+    3. same shit again?
+>>3
+    u blind bro?nope
+"""
+#daim boi, i had my money on you not being able to complete this.
