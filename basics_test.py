6adc49a9a499a2f95795c874a789f46982901863
doubl-u <69413572+doubl-u@users.noreply.github.com>
2021-07-07 14:30:17 +0530

    _

--- basics_test.py
+++ basics_test.py
@@ -1,17 +1,18 @@
-#all functions should run in a loop repeatedly unless the keyword "Iquit:P" is typed in at any point in time. no external online help or external libraries :)
+#all functions should run in a loop repeatedly unless the keyword "quit" is typed in at any point in time. no external online help or external libraries :)
 """
 Q_1, 
 Make a calculator using the 3 user inputs (1st number, 2nd number, and the operation needed to be sunducted between them)
 
 The user needs to be able to add, subtract, multiply, divide, raise them to a interger (powers) and root them to a integer
 
-ex. 8 of root 3 = 2
+ex. 8 of root 3 (cuberoot) = 2
+    8 + 3 = 11
 
 """
 #completed the captcha test to check if you are human.
 """
 Q_2, 
-Make a script that takes the a string from the user and switches the capitalisation of alphabets in the string alternatively(with first letter capital).
+Make a script that takes the a string from the user and switches the capitalisation of alphabets in the string alternatively (with first letter capital).
 
 ex. "hello world 123four" = "HeLlO wOrLd 123FoUr"
 
@@ -21,28 +22,36 @@ ex. "hello world 123four" = "HeLlO wOrLd 123FoUr"
 Q_3,
 Make a visual multiplication table with the square dimension of user specification (11 will make a 11x11 table).
 This table should not display any square numbers (1 is square number as 1x1 = 1, with "LoL" in its empty place).
-This table also should be positioned in the way that the smallest value is in the top-right corner and the highest value in the bottom-right corner.
+This table also should be positioned in the way that the smallest value is in the top-right corner and the highest value in the bottom-left corner.
 
 The table should only show numbers and it should have all first digits of the numbers perfectly aligned in the xy-axis, no matter even if there are numbers range from 1 to 4 digits in different places.
 (including aligning the first L of LoL)
 
 ex. with user input 4,
 
-    4    3    2    LoL
+    LoL  3    2    LoL
     8    6    LoL  2
     12   LoL  6    3
-    LoL  12   8    4
+    LoL  12   8    LoL
+
+not:
+    LoL 3 2 LoL
+    8 6 LoL 2
+    12 LoL 6 3
+    LoL 12 8 LoL
 
 """
 #was seeing whether your brain cells were intact, but impressive 
 """
 Q_4,
-Make a database for all numbers until the user specification (0 nnot included),
+Make a database for all numbers until the user specification (0 not included),
 The information should be revealed on user calling only (cannot display everything at once)
 The information stored should contain: whether if its an odd number or even number, whether if its a prime number (1 is not prime), 
 and whether or not the current number inputed is the same as the last number inquired for (its a default no on the first try)
 
-ex. with maximum set to 100,
+ex. 
+>>100
+    numbers from 1 to 100 can be searched for.
 >>3
     1. odd or even?
     2. prime number?
@@ -56,6 +65,6 @@ ex. with maximum set to 100,
 >>3
     nope
 """
-#nani? i would have never put my money on you to complete this.
+#nani? i wouldn't have put my money on you to complete this.
 
-#efficeincy score[], big brains score[], readability score[]
+#efficiency score[], big brains score[], readability score[]
